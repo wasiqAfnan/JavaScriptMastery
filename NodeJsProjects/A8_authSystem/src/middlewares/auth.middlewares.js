faadaa54dffbe8e7de9ad0d4ec11b99ae52f3321
wasiqafnan <wansari964@yahoo.com>
2025-07-12 00:27:53 +0530

    Some tweaks and small fixed in auth middleware

--- NodeJsProjects/A8_authSystem/src/middlewares/auth.middlewares.js
+++ NodeJsProjects/A8_authSystem/src/middlewares/auth.middlewares.js
@@ -10,27 +10,29 @@ export const authMiddleware = async (req, res, next) => {
             message: "Unauthorized! Please login to access dashbaboard",
         });
     }
-    // decode token
     try {
+        // decode token
         const payload = jwt.verify(token, process.env.JWT_SECRET);
+        // finding user on db based on decoded token data
         const user = await User.findOne({ _id: payload.id }).select(
             "uName uEmail _id"
         );
+        // validate user
         if (!user) {
             return res.status(403).json({
                 message: "Token not valid",
             });
         }
-
+        // if uyser exist then setting up req.user obj to pass to handler
         req.user = {
             id: user._id,
             name: user.uName,
             email: user.uEmail,
         };
+
+        return next();
     } catch (error) {
         console.error("Auth error:", error);
         return res.status(403).json({ message: "Invalid or expired token" });
-    } finally {
-        next();
     }
 };
